94ad9058a99bc094696432b53e5964c2ea138ba0
Carl Gulliksson <gulliksson.carl@gmail.com>
2024-04-02 17:42:27 +0200

    Added favicon and other good stuff

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function Navbar() {
   const [top, setTop] = useState("-64px");
@@ -14,18 +15,33 @@ export default function Navbar() {
       className="flex justify-between items-center h-16 w-full z-10 bg-white text-black fixed shadow-sm font-mono"
       style={{ top: top, transition: "top 0.3s" }}
     >
-      <a href="#home" className="pl-8">
+      <a href="#" className="pl-8">
         <h3>CG</h3>
       </a>
-      <div className="pr-8 md:block hidden">
-        <a href="#about" className="p-4">
+      <div className="flex items-center pr-8">
+        <a
+          href="#about"
+          className="mx-4 border-b border-b-transparent hover:border-b-black hover:border-dashed hover:border-b"
+        >
           01. About
         </a>
-        <a href="#projects" className="p-4">
+        <a
+          href="#projects"
+          className="mx-4 border-b border-b-transparent hover:border-b-black hover:border-dashed hover:border-b"
+        >
           02. Projects
         </a>
-        <a href="#contact" className="p-4">
+        <a
+          href="#contact"
+          className="mx-4 border-b border-b-transparent hover:border-b-black hover:border-dashed hover:border-b"
+        >
           03. Contact
+        </a>
+        <a href="https://github.com/carl1330" target="_blank" className="p-4">
+          <FaGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/carl-gulliksson/" target="_blank">
+          <FaLinkedin />
         </a>
       </div>
     </div>
