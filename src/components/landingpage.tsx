a0e75aa314d181e23227194052ec92a6c69e6343
Carl Gulliksson <gulliksson.carl@gmail.com>
2024-04-02 21:59:21 +0200

    yea

--- src/components/landingpage.tsx
+++ src/components/landingpage.tsx
@@ -2,7 +2,7 @@ export default function LandingPage() {
   return (
     <div className="flex h-screen w-full justify-center items-center">
       <div className="absolute overflow-hidden w-full h-full blur-md -m-3 grayscale">
-        <img src="src/assets/background.jpg" />
+        <img src="background.jpg" />
       </div>
       <div className="w-96 text-white z-10">
         <h1>Carl</h1>
